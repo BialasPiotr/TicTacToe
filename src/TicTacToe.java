c0d1505208e77c07d41be387da6d088f38217593
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2022-12-29 14:28:34 +0100

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -107,7 +107,7 @@ public class TicTacToe extends JFrame {
         }
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         new TicTacToe();
     }
 }
